5f5faff449a74a9df7914c94dbd2a3aa29186099
Dean <78488912+Dean8ix@users.noreply.github.com>
2024-09-30 23:46:14 +0100

    Update DEX.sol

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.0;
 
 contract DEX {
 
     mapping (address => uint256) balances;
 
+    event Exchanged(uint256 ethValue, uint256 dexValue);
+
     function exchange() external payable {
 
         assert(msg.value > 0);
+        
         uint amount = msg.value * 5;
 
         balances[msg.sender] += amount;
 
+        emit Exchanged(msg.value, amount);
     }
 
     function withdraw(uint256 amount) external {
